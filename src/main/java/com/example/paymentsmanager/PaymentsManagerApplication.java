a06596ecd37aed7a8e6e9c3e6571def7f2828bef
Renato Guedes <renato.guedes14@gmail.com>
2024-04-16 21:38:27 -0300

    chore: initial commit
    - Create project directories with Spring Initializr
    - Add README.md
    - Initialize git repository
    - Add .gitignore and basic configuration files

---
+++ src/main/java/com/example/paymentsmanager/PaymentsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.paymentsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/paymentsmanager/PaymentsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paymentsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
