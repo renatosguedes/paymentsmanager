12bcd26f8364cc90baae5c2d3341862d18d8fcd5
Renato Guedes <renato.guedes14@gmail.com>
2024-04-17 17:22:29 -0300

    feat: simulate 6 seconds delay when calling feign clients

--- src/main/java/com/example/paymentsmanager/configs/FeignConfig.java
+++ src/main/java/com/example/paymentsmanager/configs/FeignConfig.java
@@ -3,6 +3,7 @@ package com.example.paymentsmanager.configs;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
+import feign.RequestInterceptor;
 import feign.Util;
 import feign.codec.Decoder;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +18,18 @@ public class FeignConfig {
             String bodyStr = Util.toString(response.body().asReader(Util.UTF_8));
             JavaType javaType = TypeFactory.defaultInstance().constructType(type);
             return new ObjectMapper().readValue(bodyStr, javaType);
+        };
+    }
+
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return requestTemplate -> {
+            try {
+                Thread.sleep(6000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException("Delay interrupted", e);
+            }
         };
     }
 }
