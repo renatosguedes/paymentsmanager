a51396d473875d6f009258d871759ff77e9041f8
Renato Guedes <renato.guedes14@gmail.com>
2024-04-17 21:04:36 -0300

    feat: enable caching
    It's now possible to cache the external API's endpoints, in order to respond faster should the user wants to process the payments they just fetched. It is also faster when the user retrieves the list of flows

--- src/main/java/com/example/paymentsmanager/PaymentsManagerApplication.java
+++ src/main/java/com/example/paymentsmanager/PaymentsManagerApplication.java
@@ -2,12 +2,14 @@ package com.example.paymentsmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableFeignClients
+@EnableCaching
 public class PaymentsManagerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/paymentsmanager/datasources/SistemaFluxosClient.java
+++ src/main/java/com/example/paymentsmanager/datasources/SistemaFluxosClient.java
@@ -3,6 +3,7 @@ package com.example.paymentsmanager.datasources;
 import com.example.paymentsmanager.configs.FeignConfig;
 import com.example.paymentsmanager.entities.dtos.GetFluxoByIdResponse;
 import com.example.paymentsmanager.entities.dtos.GetFluxosResponse;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,9 +15,11 @@ import static com.example.paymentsmanager.utils.ConstantsUtil.CLIENT_URL;
 public interface SistemaFluxosClient {
 
     @GetMapping("/fluxos.json")
+    @Cacheable("fluxos")
     GetFluxosResponse getFluxos();
 
     @GetMapping("/fluxos/{id}.json")
+    @Cacheable("fluxoById")
     GetFluxoByIdResponse getFluxoById(@PathVariable String id);
 
 }
